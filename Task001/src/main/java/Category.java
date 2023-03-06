9f585607357b6a6c2f4e0456ac5f56a76c6b6a68
Slavchik Slavili <slavchikv@rambler.ru>
2023-03-06 15:59:54 +0700

    Реализация класса Category(Категории)

---
+++ Task001/src/main/java/Category.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+/**
+ *  Класс Категория товаров
+ */
+public class Category {
+
+
+    private String categoryName;
+
+    private ArrayList<Product> productList;
+
+    public Category(String categoryName, ArrayList<Product> productList) {
+        this.categoryName = categoryName;
+        this.productList = productList;
+    }
+
+    public Category(String categoryName, Product userProduct) {
+        this.categoryName = categoryName;
+        this.productList = new ArrayList<>();
+        this.productList.add(userProduct);
+    }
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+        this.productList = new ArrayList<>();
+    }
+
+    /**
+     * Получение полного списка товаров данной категории
+     * @return
+     */
+    public ArrayList<Product> getProductList() {
+        return productList;
+    }
+
+    /**
+     * Добавление отдельного товара
+     * @param userProduct
+     * @return - true - успешно добавлен, false - товар не добавлен
+     */
+    public boolean addIteimIntoProductList(Product userProduct) {
+        return this.productList.add(userProduct);
+    }
+
+    /**
+     * Удаление товара из списка по его имени
+     * @param nameOfProduct - имя товара
+     * @return - true - товар успешно удалён, false - не удалён
+     */
+    public boolean removeIteimFromProductList(String nameOfProduct) {
+        return this.productList.removeIf(product -> (product.getProductName()==nameOfProduct));
+    }
+
+    /**
+     * Получение имени категории
+     * @return
+     */
+    public String getCategoryName() {
+        return categoryName;
+    }
+}
--- Task001/src/main/java/Program.java
+++ Task001/src/main/java/Program.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
@@ -6,12 +7,28 @@ public class Program {
         ArrayList<Product> allProducts = new ArrayList<Product>();
         allProducts.add(new Product("Товар 1", 100, 10, 15));
         allProducts.add(new Product("Товар 2", 99, 8, 10));
-        allProducts.add(new Product("BMW", 6000000, 8, 0));
+        //allProducts.add(new Product("BMW", 6000000, 8, 0));
+        //allProducts.removeIf(product -> (product.getProductName()=="BMW"));
 
-        for (var item: allProducts) {
-            System.out.println(item.allProductInformation());
+
+
+        //Category category1 = new Category("Разный товар", allProducts);
+        //Category category1 = new Category("Cars", new Product("AUDI", 5000000, 8, 0));
+        Category category1 = new Category("Cars");
+        category1.addIteimIntoProductList(new Product("BMW", 6000000, 8, 0));
+        category1.addIteimIntoProductList(new Product("AUDI", 5000000, 8, 0));
+
+        System.out.println(category1.getCategoryName());
+
+        for (var item: category1.getProductList()) {
+            System.out.println(((Product)item).allProductInformation());
         }
+        System.out.println(category1.removeIteimFromProductList("AUDI"));
 
+        System.out.println("-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-__");
 
+        for (var item: category1.getProductList()) {
+            System.out.println(((Product)item).allProductInformation());
+        }
     }
 }
