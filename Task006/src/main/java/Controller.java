6e67774e2d5789e012f75a9564e2bd1b5c36d6ba
Slavchik Slavili <slavchikv@rambler.ru>
2023-03-24 15:10:29 +0700

    Доработка классов: controller и view

--- Task006/src/main/java/Controller.java
+++ Task006/src/main/java/Controller.java
@@ -2,27 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
+    private StudyGroupService groups;
+    private View view;
+    public Controller(){
+        this.view = new View(new StudyGroupService());
+    }
+
+    public void setGroups(){
+        this.view.setStudyGroups();
+    }
+
     public void viewGroup(){
-        StudyGroupService groups = new StudyGroupService();
-
-
-
-        for (int i = 1; i <= 5; i++) {
-            List<String> tempStudentsList = new ArrayList<>();
-            String tempTeacher = String.format("Teacher №%d", i);
-            for (int j = 1; j <=10; j++) {
-                tempStudentsList.add(String.format("Student №%d", j));
-            }
-            groups.addGroup(tempTeacher, tempStudentsList);
-
-        }
-
-        for (var parentItem: groups.getGroups()) {
-            System.out.println(parentItem.getTeacher());
-            for (int i = 0; i < parentItem.getStudents().toArray().length; i++) {
-                System.out.println("\t"+parentItem.getStudents().get(i));
-            }
-            System.out.println("--------------------------------------");
-        }
+        this.view.getStudyGroups();
     }
 }
--- Task006/src/main/java/Program.java
+++ Task006/src/main/java/Program.java
@@ -1,21 +1,11 @@
 public class Program {
     public static void main(String[] args) {
-        /*
+
         Controller group = new Controller();
+        group.setGroups();
+
+
         group.viewGroup();
-
-         */
-
-        Staff teacher = new Staff("Иванов Иван Иваныч", 1,3);
-        System.out.println("Преподаватель:");
-        System.out.println(teacher.getFullName());
-        System.out.println(teacher.getTeachingExperience());
-
-
-        Student std = new Student("Петров Санёк",1, "5ИИТ-71", 1);
-        System.out.println("Студент:");
-        System.out.println(std.getFullName());
-        System.out.println(std.getNameOfGroup());
 
 
     }
